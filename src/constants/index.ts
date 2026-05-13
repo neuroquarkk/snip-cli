e8265e2a9d0e84c0762369133ebd92bd1182e15f
nqu <202017450+neuroquarkk@users.noreply.github.com>
2026-05-13 21:42:58 +0530

    simplify BASE_URL env access

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,7 @@ import { join } from 'path';
 import { homedir } from 'os';
 
 export const CONSTANTS = {
-    BASE_URL: process.env['BASE_URL'] || 'http://localhost:8080/api/v1',
+    BASE_URL: process.env.BASE_URL || 'http://localhost:8080/api/v1',
 
     CRED_FILE: join(homedir(), '.sniprc'),
 
